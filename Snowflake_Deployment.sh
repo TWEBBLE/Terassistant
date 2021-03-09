b0a39b4d9a35e89026919a60736332c63897fa2c
TWEBBLE <tomwebb777@gmail.com>
2021-03-09 15:06:07 +0000

    Snowflake_Deployment.sh committed

--- Snowflake_Deployment.sh
+++ Snowflake_Deployment.sh
@@ -1,11 +1,11 @@
-set ENV=DEV
-echo ENV
+export ENV=DEV
+echo deploying environment $ENV
 python3 query_snowflake.py
 
-set ENV=STAGING
-echo deploying environment ENV
+export ENV=STAGING
+echo deploying environment $ENV
 python3 query_snowflake.py
 
-set ENV=PROD
-echo deploying environment ENV
+export ENV=PROD
+echo deploying environment $ENV
 python3 query_snowflake.py
