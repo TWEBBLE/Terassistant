d73d9f26c715c6589a9609be0321fda96a77ac23
TWEBBLE <tomwebb777@gmail.com>
2021-03-11 12:33:45 +0000

    CSV Date cleaned....Query_Snowflake updated column headers

--- Query_Snowflake.py
+++ Query_Snowflake.py
@@ -11,13 +11,23 @@ snowflake_connection_details = {
 
 snowflake_instance = Snowflake_Connector(snowflake_connection_details)
 cursor = snowflake_instance.set_session_parameters( role="SYSADMIN", warehouse="COMPUTE_WH")
-
 create_db_world_cups = snowflake_instance.run_sql(
     cursor, f"CREATE DATABASE IF NOT EXISTS World_Cups_{os.environ.get('ENV', 'DEV')};")
-create_schema_matches = snowflake_instance.run_sql(cursor, f"CREATE SCHEMA IF NOT EXISTS World_Cups_{os.environ.get('ENV')}.Matches;")
-# create_table_Brazil_2014 = snowflake_instance.run_sql(cursor, f"CREATE TABLE IF NOT EXISTS World_Cups_{os.environ.get('ENV')}.Matches.Brazil_2014 ....;")
-# data_putter = snowflake_instance.run_sql(cursor, f"PUT <CSV file> @%test_table......;")
-# data_copier = snowflake_instance.run_sql(cursor, f"COPY INTO test_table;")
-
 result = snowflake_instance.run_sql(cursor, "SHOW DATABASES;")
 df = snowflake_instance.fetch_dataframe_from_sql(cursor, "SHOW DATABASES;")
+print(df)
+
+create_schema_matches = snowflake_instance.run_sql(
+    cursor, f"CREATE SCHEMA IF NOT EXISTS World_Cups_{os.environ.get('ENV', 'DEV')}.Matches;")
+
+table_ddl_statement =''' "Year" INTEGER, "Date" DATE, "Time" TIME, "Stage" VARCHAR(50), "Stadium" VARCHAR(50), "City" VARCHAR(50), "Home_Team_Name" VARCHAR(50), "Home_Team_Goals" INTEGER, "Away_Team_Goals" INTEGER, "Away_Team_Name" VARCHAR(50), "Win_conditions" VARCHAR(50), "Attendance" INTEGER, "Half_time_Home_Goals" INTEGER, "Half_time_Away_Goals" INTEGER, "Referee" VARCHAR(50), "Assistant_1" VARCHAR(50), "Assistant_2" VARCHAR(50), "RoundID" INTEGER, "MatchID" INTEGER, "Home_Team_Initials" VARCHAR(5), "Away_Team_Initials" VARCHAR(5) '''
+create_table_Brazil_2014 = snowflake_instance.run_sql(
+    cursor, f"CREATE TABLE IF NOT EXISTS World_Cups_{os.environ.get('ENV', 'DEV')}.Matches.Brazil_2014 ({table_ddl_statement});")
+data_putter = snowflake_instance.run_sql(
+    cursor, f'PUT file://Data\WorldCupMatches.csv @~ auto_compress=false;')
+read = snowflake_instance.run_sql(cursor, f"LIST @~")
+print(read)
+
+data_copier = snowflake_instance.run_sql(
+cursor, f"""COPY INTO World_Cups_{os.environ.get('ENV', 'DEV')}.Matches.Brazil_2014 from @~/WorldCupMatches.csv FILE_FORMAT = (TYPE = 'csv' FIELD_OPTIONALLY_ENCLOSED_BY = '"' SKIP_HEADER = 1);""")
+
